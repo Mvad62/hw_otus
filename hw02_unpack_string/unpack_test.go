61a13c46612b3bbd12bc0489fe933d861dcda493
Mvad <mvad@hotmail.com>
2026-08-19 17:17:25 +0300

    HW2 is completed

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -19,10 +19,17 @@ func TestUnpack(t *testing.T) {
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `a\03b`, expected: `a000b`},
+		// added:
+		{input: `\1abc`, expected: `1abc`},
+		{input: `a\10b`, expected: `ab`},
+		{input: `a0b2`, expected: `bb`},
+		{input: `a\\2b`, expected: `a\\b`},
+		{input: `a\12`, expected: `a11`},
 	}
 
 	for _, tc := range tests {
@@ -36,7 +43,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "c23ab", `qw\ne`, `\a`, `qwe\`, `\`, "a12b", "a05"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
