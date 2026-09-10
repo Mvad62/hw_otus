c026600cab25d5d5a9e195df89ca1a694cc1d755
Mvad62 <mvad@hotmail.com>
2026-09-10 16:47:51 +0300

    go.uber.org/goleak deleted

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -10,12 +10,9 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"go.uber.org/goleak"
 )
 
 func TestRun(t *testing.T) {
-	defer goleak.VerifyNone(t)
-
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
 		tasksCount := 50
 		tasks := make([]Task, 0, tasksCount)
@@ -88,8 +85,6 @@ func TestRun(t *testing.T) {
 	})
 
 	t.Run("concurrency without sleep", func(t *testing.T) {
-		defer goleak.VerifyNone(t)
-
 		const (
 			workers    = 4
 			tasksCount = 10
